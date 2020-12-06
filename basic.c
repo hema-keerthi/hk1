3323dd855d25b21a5aa1aa5945210ff2af21eba7
hema-keerthi <75245829+hema-keerthi@users.noreply.github.com>
2020-12-06 20:11:46 +0530

    added product of 2 numbers

--- basic.c
+++ basic.c
@@ -4,4 +4,5 @@ void main()
   int a=10,b=5;
   printf("sum of the 2 numbers is : %d ",(a+b));
   printf("difference of two numbers is : %d ",(a-b));
+  printf("The product of these 2 numbers is : %d ",(a*b));
 }
