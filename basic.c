9004e4724c720770860c840cd47e223e050b70b3
hema-keerthi <75245829+hema-keerthi@users.noreply.github.com>
2021-07-10 15:02:14 +0530

    Delete basic.c

--- basic.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-  int a=10,b=5;
-  printf("sum of the 2 numbers is : %d ",(a+b));
-  printf("difference of two numbers is : %d ",(a-b));
-  printf("The product of these 2 numbers is : %d ",(a*b));
-  printf("The factorial of the two numbers are : 10!=3628800 , 5!= 120 ");
-}
